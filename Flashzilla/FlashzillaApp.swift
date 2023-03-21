553c461c30376da634d8a6ffce99781763f1251c
Maria Romanchuk <mashulya_r_88@mail.ru>
2023-03-21 21:04:55 +0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Мария Романчук on 21.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
